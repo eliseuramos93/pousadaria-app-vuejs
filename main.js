5b802620f8c0e517c347c8449c2f56f052c45e4c
Eliseu Ramos <eliseu.mec@gmail.com>
2023-11-26 16:41:37 -0300

    add three endpoints accesses to app
    - first endpoint: search inns
      list all of the rooms with the
      provided text filtering the list
      by name. if no text is given, return
      all inns.
    
    - second endpoint: get inn details
      by clicking on an inn name, get all
      of the inn's details and a link to
      request all of the inn's rooms
    
    - third endpoint: get rooms list
      request all of the inn's rooms, and
      shows a form to check if a room is
      available or not.

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const app = Vue.createApp({
   data(){
     return {
+      // getInns data
       searchText: '',
-      innId: '',
+      listInns: [],
+      hideInnsList: true,
+      
+      // showInnDetails data
+      hideInnDetails: true,
       brandName: '',
       averageRating: '',
       description: '',
       policy: '',
       phoneNumber: '',
+      petFriendly: '',
       streetName: '',
       number: '',
       complement: '',
@@ -15,49 +21,98 @@ const app = Vue.createApp({
       city: '',
       state: '',
       zipCode: '',
-      listInns: []
-    }
-  },
 
-  async mounted(){
-    this.listResult = await this.getInns()
+      // getRooms data
+      hideRoomsList: true,
+      currentInnId: '',
+      listRooms: [],
+
+      // checkRoomAvailability data
+      hideReservationDetails: true,
+      checkinDate: '',
+      checkoutDate: '',
+      numberGuests: '',
+      reservationPrice: '',
+      errors: [],
+    }
   },
 
   methods: {
     async getInns(){
-      let response = await fetch('http://localhost:3000/api/v1/inns');
+      this.hideInnsList = false;
+      this.hideInnDetails = true;
+      this.hideRoomsList = true;
+
+      this.currentInnId = '';
+
+      let url = ''
+      if(this.searchText) {
+        url = `http://localhost:3000/api/v1/inns/?name=${this.searchText}`
+      } else {
+        url = `http://localhost:3000/api/v1/inns/`
+      }
+
+      let response = await fetch(url);
       let data = await response.json();
       
       this.listInns = [];
       data.forEach(item => {
         var inn = new Object();
-        inn.brandName = item.brand_name;
-        inn.averageRating = item.average_rating;
-        inn.description = item.description;
-        inn.policy = item.policy;
-        inn.phoneNumber = item.phone_number;
-        inn.streetName = item.address.street_name;
-        inn.number = item.address.number;
-        inn.complement = item.address.complement;
-        inn.neighborhood = item.address.neighborhood;
-        inn.city = item.address.city;
-        inn.state = item.address.state;
-        inn.zipCode = item.address.zip_code;
+        inn.id = item.id;
+        inn.name = item.brand_name;
 
         this.listInns.push(inn)
       })
     },
-  },
 
-  computed: {
-    listResult(){
-      if(this.searchText) {
-        return this.listInns.filter(inn => {
-          return inn.brandName.toLowerCase().includes(this.searchText.toLowerCase());
-        })
-      } else {
-        return this.listInns;
-      }
+    async getInnDetails(innId){
+      this.hideInnsList = true;
+      this.hideInnDetails = false;
+
+      let response = await fetch(`http://localhost:3000/api/v1/inns/${innId}/`)
+      let data = await response.json();
+      
+      this.currentInnId = innId
+      this.brandName = data.brand_name;
+      this.averageRating = data.average_rating;
+      this.description = data.description;
+      this.policy = data.policy;
+      this.phoneNumber = data.phone_number;
+      this.petFriendly = data.pet_friendly
+      this.streetName = data.address.street_name;
+      this.number = data.address.number;
+      this.complement = data.address.complement;
+      this.neighborhood = data.address.neighborhood;
+      this.city = data.address.city;
+      this.state = data.address.state;
+      this.zipCode = data.address.zip_code;
+    },
+
+    async getRooms(innId){
+      this.hideRoomsList = false;
+
+      let response = await fetch(`http://localhost:3000/api/v1/inns/${innId}/rooms`)
+      let data = await response.json();
+
+      this.listRooms = [];
+      data.forEach(item => {
+        var room = new Object();
+        room.id = item.id;
+        room.name = item.name;
+        room.description = item.description;
+        room.area = item.area
+        room.maxCapacity = item.max_capacity;
+        room.rentPrice = item.rent_price;
+        room.hasBathroom = item.has_bathroom;
+        room.hasBalcony = item.has_balcony;
+        room.hasAirConditioner = item.has_air_conditioner;
+        room.hasTV = item.has_tv;
+        room.hasWardrobe = item.has_wardrobe;
+        room.hasVault = item.has_vault;
+        room.isAccessible = item.is_accessible;
+
+        this.listRooms.push(room)
+      });
     }
   }
 });
